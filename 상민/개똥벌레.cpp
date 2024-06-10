cba34157b09b23c1a0c91f117dd56c9056f45458
jsangmin99 <jsangmin99@gmail.com>
2024-06-11 00:35:09 +0900

    개똥벌레 누적합 설명추가

--- 상민/개똥벌레.cpp
+++ 상민/개똥벌레.cpp
@@ -20,10 +20,21 @@ int main() {
         bottomup[a]++;
         topdown[h + 1 - b]++;
     }
-    
+    //확인  
+    for(int i = 0 ; i <= h; i++){
+        cout << i <<"짜리 석순" << bottomup[i] << "\n";
+    }
+    for(int i = 0 ; i <= h; i++){
+        cout << i << "이상높이 종유석 : " << topdown[i] << "\n";
+    }
+
     // 종유석 누적합 계산 (아래에서 위로)
     for (int i = 1; i <= h; ++i) {
         topdown[i] += topdown[i - 1];
+    }
+    //확
+    for(int i = 0 ; i <= h; i++){
+        cout << i << "이상높이 종유석 : " << topdown[i] << "\n";    
     }
 
     // 석순 누적합 계산 (위에서 아래로)
@@ -31,9 +42,19 @@ int main() {
         bottomup[i] += bottomup[i + 1];
     }
 
+    //확인 
+    for(int i = 0 ; i <= h; i++){
+        cout << i <<"짜리 석순" << bottomup[i] << "\n";
+    }
+
+
     // 높이에 따른 부딪힘 계산
     for (int i = 1; i <= h; ++i) {
         hits[i] = topdown[i] + bottomup[i];
+    }
+
+    for(int i = 0 ; i <= h; i++){
+        cout << i << "칸에 있는 : " << hits[i] << "\n";    
     }
 
     // 최솟값 찾기
